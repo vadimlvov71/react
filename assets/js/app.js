d51d62b3f3d5996c5764bbc8126d698825606115
vadimlvov71 <57807117+vadimlvov71@users.noreply.github.com>
2021-08-26 20:03:13 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 //import '../styles/app.css';
 import Home from './components/Home';
-//import Pagination from './components/Pagination';
+import Users from './components/Users';
+ //const location = useLocation();
+  //console.log(location.pathname);
 /*
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Hello</p>
-      </div>
-    )
-  }
-} 
-* */
 try {   
 	ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
 	console.log('rendered');
 } catch (e) {
   console.log(e);
 }
+*/
+ReactDOM.render(<Router><Switch><Route exact path="/" component={Home}/><Route exact path="/:item" component={Users}/></Switch></Router>, document.getElementById('root'));
+/*class App extends React.Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch>
+				  <Route path="/users" component={Users}/>
+				  <Route path="/" component={Home}/>
+				</Switch>
+			</BrowserRouter>
+		);
+	  }
+  }*/
